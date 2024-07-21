138f8a4043879f2f5f56c1333b83ff56bc1fa728
Waltrick <118665499+solomonerous@users.noreply.github.com>
2024-07-21 13:44:50 +0700

    Update domainSSL.sh

--- domainSSL.sh
+++ domainSSL.sh
@@ -16,19 +16,20 @@ if ls ./.lego/certificates | grep "$domain"
     cp ./.lego/certificates/$domain.key /home/ssl/$domain/key.pem
     path1="/home/ssl/$domain/cert.pem"
     path2="/home/ssl/$domain/key.pem"
-    echo "'Get SSL cho domain thành công !'
-        'Cert lưu tại đường dẫn: '$path1'.'
-        'Key lưu tại đường dẫn: '$path2'.'"
+    echo "\033[32m[SUCCEED]\033[0m Get SSL cho domain thành công !
+          \033[32m[LOG]\033[0m CERT saved at: '$path1'.
+          \033[32m[LOG]\033[0m KEY saved at: '$path2'.
+         "
 else
-    echo " 
-        [ERROR]: GET SSL FAILED !
-        [DEBUG]: 
-        3 STEPs:
-        1. Kiểm tra lại tên miền đã điền chính xác hay chưa .
-        2. Hãy kiểm tra lại port 80 có bận không, kiểm tra bằng lệnh dưới đây
-        ' lsof -i :80 '
-        Dùng lệnh "kill [PID của tiến trình]" để tắt tiến trình đang chạy tại cổng 80 rồi chạy lại script này.
-        3. Cấu hình phân giải tên miền (DNS) lỗi, kiểm tra lại cấu hình DNS.
-        "
+    echo "
+          [ERROR]: GET SSL FAILED !
+          [DEBUG]: 
+          3 STEPs:
+          1. Kiểm tra lại tên miền đã điền chính xác hay chưa .
+          2. Hãy kiểm tra lại port 80 có bận không, kiểm tra bằng lệnh dưới đây
+          ' lsof -i :80 '
+          Dùng lệnh "kill [PID của tiến trình]" để tắt tiến trình đang chạy tại cổng 80 rồi chạy lại script này.
+          3. Cấu hình phân giải tên miền (DNS) lỗi, kiểm tra lại cấu hình DNS.
+         "
 fi
 echo ' # Credit: @solomonerous | MINH '
